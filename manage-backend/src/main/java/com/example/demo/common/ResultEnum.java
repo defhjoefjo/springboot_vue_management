0a426daa683d37a82364973684ddd1c1310db701
defhjoefjo <pipipi527@gmail.com>
2021-10-27 14:19:07 +0800

    outline of the back end

---
+++ manage-backend/src/main/java/com/example/demo/common/ResultEnum.java
@@ -0,0 +1,18 @@
+package com.example.demo.common;
+
+public enum ResultEnum {
+
+    SUCCESS(200),
+
+    FAIL(400),
+
+    NOT_FOUND(404),
+
+    INTERNAL_SERVER_ERROR(500);
+
+    public int code;
+
+    ResultEnum(int code) {
+        this.code = code;
+    }
+}
