3220faecd6c27a6ad11984dd17dfb5d65c7346c4
defhjoefjo <pipipi527@gmail.com>
2021-10-27 10:52:20 +0800

    outline of the front end

---
+++ src/components/menu.vue
@@ -0,0 +1,43 @@
+<template>
+      <el-menu
+          style="width: 200px; min-height: calc(100vh - 50px)"
+          default-active="2"
+          class="el-menu-vertical-demo">
+        <el-sub-menu index="1">
+          <template #title>
+            <i class="el-icon-location"></i>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item one</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span>Navigator Two</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-setting"></i>
+          <span>Navigator Three</span>
+        </el-menu-item>
+      </el-menu>
+
+</template>
+
+<script>
+export default {
+  name: "Menu_me"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,74 @@
+<template>
+  <div style="padding: 20px">
+    <el-input v-model="input" placeholder="type to search" style="width: 20%; margin-bottom: 10px" />
+    <el-button style="margin: 5px 10px"> Search</el-button>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="date" label="Date" />
+      <el-table-column prop="name" label="Name" />
+      <el-table-column prop="address" label="Address" />
+      <el-table-column label="Operations">
+        <template #default="scope">
+          <el-button type="text" size="small" @click="handleEdit"
+          >Edit</el-button
+          >
+          <el-popconfirm
+              confirm-button-text="OK"
+              cancel-button-text="No, Thanks"
+              icon="el-icon-info"
+              icon-color="red"
+              title="Are you sure to delete this?"
+          >
+            <template #reference>
+              <el-button type="text" size="small">Delete</el-button>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="margin: 10px 0">
+      <el-pagination
+          v-model:currentPage="currentPage1"
+          :page-sizes="[5,10,15,20]"
+          :page-size="10"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+      >
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+  },
+  data() {
+    return {
+      tableData: [{
+        date: "123",
+        name: "shabi",
+        address: "sss",
+      }],
+      currentPage1:0,
+      total:100,
+    }
+  },
+  methods: {
+    handleEdit() {
+
+    },
+    handleSizeChange() {
+
+    },
+    handleCurrentChange(){
+
+    }
+  }
+},
+);
+</script>
